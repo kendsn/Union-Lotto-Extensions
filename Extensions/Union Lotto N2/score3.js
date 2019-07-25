cef409d2b81ff333adf562eb83a3058874db715d
睏睏 <kendsng@gmail.com>
2019-07-25 13:20:31 +0800

    time 30sec
    change 5sec

--- Extensions/Union Lotto N2/score3.js
+++ Extensions/Union Lotto N2/score3.js
@@ -77,7 +77,7 @@ function parse() {
 	}
 	console.log("Ret>>")
 	console.log(ret);
-	setTimeout(function() {location.reload();}, 30000);
+	setTimeout(function() {location.reload();}, 5000);
 }
 
 function alert(msg) {}
--- Extensions/Union Lotto N2/score4.js
+++ Extensions/Union Lotto N2/score4.js
@@ -80,7 +80,7 @@ function parse() {
 	}
 	console.log("Ret>>")
 	console.log(ret);
-	setTimeout(function() {location.reload();}, 10000);
+	setTimeout(function() {location.reload();}, 5000);
 }
 
 function alert(msg) {}
